bda3255c4b39528ae31b5fd751b4331cbb799337
whyj107 <60024292+whyj107@users.noreply.github.com>
2021-08-14 19:55:46 +0900

    Vue Study
    Google Map Tutorial

---
+++ 2021/20210814/src/main.js
@@ -0,0 +1,18 @@
+import Vue from 'vue'
+import App from './App.vue'
+
+import * as VueGoogleMaps from "vue2-google-maps" // Import package
+
+Vue.config.productionTip = false
+
+Vue.use(VueGoogleMaps, {
+  load: {
+    key: "AIzaSyA9tTRekdZ-uxZGCF6BeQdD8bZcbdigQNk",
+    libraries: "places",
+    region: "KR" //이거 추가 안하면 동해가 일본해로 뜸 ㅡ"ㅡ
+  }
+});
+
+new Vue({
+  render: h => h(App),
+}).$mount('#app')
